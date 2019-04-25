0f9c5701e3dfd591196956541dd8be938da50410
Ivan <slai@inbox.ru>
2019-04-25 22:02:38 +0900

    Это наш первый комит

---
+++ proekt/scripts.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+var LeftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(second);
+
+//alert ('Hellow World!');
+//let answer = confirm ('How are you?');
+
+//console.log(answer);
+
+let answer = prompt ('Вам 18?','Да');
+
+console.log(answer);
